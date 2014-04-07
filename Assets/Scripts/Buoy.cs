4f9396d02eb3f0a7e805b182842ad195ec9a33d8
Kristian Johansson <morfah@gmail.com>
2014-04-07 22:50:58 +0200

    Tweaked the sprites a bit and added Buoy script file.

---
+++ Assets/Scripts/Buoy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buoy : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+//		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider collider){
+		if (collider.tag == "Player"){
+			Debug.Log ("Player went inside the Buoy trigger!");
+		}
+	}
+
+	void OnTriggerExit (Collider collider){
+		if (collider.tag == "Player"){
+			Debug.Log ("Player went outside the Buoy trigger!");
+		}
+	}
+}
--- Assets/Scripts/VisualExperiencePoint.cs
+++ Assets/Scripts/VisualExperiencePoint.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class VisualExperiencePoint : MonoBehaviour {
 	float state1time = 1f; // Move towards Player
-	float state2time = 2f; // Destroy self
+	float state2time = 1.5f; // Destroy self
 	float spawnedTime;
 	int state = 0;
 	float randomposX, randomposY, randomposZ;
@@ -24,13 +24,13 @@ public class VisualExperiencePoint : MonoBehaviour {
 		if (state == 0) { // Move to random position
 			transform.position = Vector3.Lerp (transform.position, 
                                    new Vector3 (randomposX, randomposY, randomposZ), 
-                                   Time.deltaTime * 1.5f);
+                                   Time.deltaTime * Random.Range(1f, 10f));
 			transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform.position); // face the camera
 		} else if (state == 1) { // Move towards player
 			GameObject player = GameObject.FindGameObjectWithTag ("Player");
 			transform.position = Vector3.Lerp (transform.position, 
                                    player.transform.position, 
-                                   Time.deltaTime * 3f);
+                                   Time.deltaTime * 10f);
 			transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform.position); // face the camera
 			//transform.renderer.material.color.a = Mathf.Lerp(1.0f,0.0f,Time.deltaTime * 3f);
 		} else if (state == 2) { // Destroy self
