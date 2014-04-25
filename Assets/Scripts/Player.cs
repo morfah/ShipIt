6498a44b67d8587426e1d57b83845773311e8ff5
Kristian Johansson <morfah@gmail.com>
2014-04-25 22:42:04 +0200

    Press Q to toggle camera modes. There's Third person and Top-Down mode. Maybe we can have third person in space and top-down on indoors levels by default.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,10 +26,13 @@ public class Player : MonoBehaviour {
 	private bool Fire1;
 	private bool Boost;
 	private float look;
+	private bool ToggleCamera;
+	private Transform cam;
 
 	// Use this for initialization
 	void Start () {
 		Screen.lockCursor = true; // so that the mouse wont escape the window
+		cam = Camera.main.camera.transform;
 	}
 	
 	// Update is called once per frame
@@ -49,6 +52,10 @@ public class Player : MonoBehaviour {
 
 		//TODO add Fire2 code
 
+		if (ToggleCamera) {
+			ToggleCameraAngle();
+		}
+
 
 //		GameObject.FindGameObjectsWithTag("ExpPoint").
 //		exppoint.transform.position(GameObject.FindGameObjectWithTag("Player"));
@@ -64,6 +71,7 @@ public class Player : MonoBehaviour {
 		//bool FlyUp = Input.GetButton("FlyUp");
 		//bool FlyDown = Input.GetButton("FlyDown");
 		Boost = Input.GetButton("Boost");
+		ToggleCamera = Input.GetButtonUp ("ToggleCameraMode");
 		
 		MovementSpeedBonus = Boost ? SpeedBoostMultiplier : 1f;
 		
@@ -98,6 +106,28 @@ public class Player : MonoBehaviour {
 		if (hp <= 0) {
 			GUI.Box(new Rect(Screen.width / 2-50, 50, 100, 20), 
 			        "\"Dead\"");
+		}
+	}
+
+	void ToggleCameraAngle(){
+		Debug.Log (cam.localPosition);
+		Debug.Log (cam.localEulerAngles);
+		Debug.Log (cam.localScale);
+
+//		(0.0, 8.4, -6.5)
+//		(24.7, 0.0, 0.0)
+//		(1.0, 1.0, 1.0)
+
+		if (cam.localPosition != new Vector3(0.0f, 50.0f, 15.0f)) {
+			cam.localPosition = new Vector3 (0.0f, 50.0f, 15.0f);
+			cam.localEulerAngles = new Vector3 (80.0f, 0.0f, 0.0f);
+			cam.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+			Debug.Log("TOP-DOWN VIEW");
+		} else {
+			cam.localPosition = new Vector3 (0.0f, 8.4f, -6.5f);
+			cam.localEulerAngles = new Vector3 (24.7f, 0.0f, 0.0f);
+			cam.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+			Debug.Log ("TPS VIEW");
 		}
 	}
 }
