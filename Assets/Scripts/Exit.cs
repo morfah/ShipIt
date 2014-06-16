ffdefb4f6c0da6b1d6189e006a35dda9c0e34555
Kristian Johansson <morfah@gmail.com>
2014-06-17 01:21:27 +0200

    Made a _mapExit prefab that takes player to next level. Official maps will have the names map01, map02 etc etc. Fixed micro asteroid prefabs. Kuroto forgot to apply his changes to them.

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider collider){
+		if (collider.tag == "Player") {
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,15 +14,19 @@ public class MainMenu : MonoBehaviour {
 	}
 
 	void OnGUI(){
+
 		if (GUI.Button (new Rect (Screen.width / 2 - 75, 100, 150, 75), "Goto Test01")) {
-			Application.LoadLevel("Test01");
+			Application.LoadLevel ("Test01");
 		}
-		else if(GUI.Button (new Rect (Screen.width / 2 - 75, 200, 150, 75), "Goto kuroto_test")){
-			Application.LoadLevel("kuroto_test");
+		else if (GUI.Button (new Rect (Screen.width / 2 - 75, 200, 150, 75), "Goto kuroto_test")) {
+			Application.LoadLevel ("kuroto_test");
 		}
-		else if(GUI.Button (new Rect (Screen.width - 75, 0, 75, 50), "Quit")){
+		else if (GUI.Button (new Rect (Screen.width / 2 - 75, 300, 150, 75), "map01")) {
+			Application.LoadLevel("map01");
+		}
+		else if(GUI.Button (new Rect (Screen.width - 75, 0, 75, 50), "Quit")) {
 			print ("You clicked Quit Game!");
-			Application.Quit();
 		}
+
 	}
 }
