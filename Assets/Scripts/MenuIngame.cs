c1ec09549a796985527648c4e4763a6cf01c16a4
Kristian Johansson <morfah@gmail.com>
2014-06-25 19:35:49 +0200

    Rewrote main menu code. It still doesn't work though.

--- Assets/Scripts/MenuIngame.cs
+++ Assets/Scripts/MenuIngame.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IngameMenu : MonoBehaviour {
+public class MenuIngame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/MenuMain.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMain : MonoBehaviour {
+	string[] buttonTexts = new string[]
+	{
+		"Continue",
+		"New Game",
+		"Load Game",
+		"Options",
+		"Quit Game"
+	};
+	
+	void OnGUI() {
+		float buttonWidth = Screen.width / 5;
+		float buttonHeight = Screen.height / 10;
+		float buttonCenter = (Screen.width / 2) - (buttonWidth / 2);
+		float buttonSpacing = buttonHeight / 5;
+		float buttonDisplace = Screen.height / buttonTexts.Length;
+
+		// Buttons
+		for (int i = 0; i < buttonTexts.Length; i++) {
+			float buttonVerticalPos = buttonDisplace + (i*buttonHeight) + (buttonSpacing * i);
+			if (GUI.Button (new Rect (buttonCenter, buttonVerticalPos, buttonWidth, buttonHeight), buttonTexts[i]))
+			{
+				switch (i) {
+				case 0:
+					Debug.Log ("Clicked Continue");
+					break;
+				case 1:
+					Debug.Log ("Clicked New Game");
+					Application.LoadLevel("map01");
+					break;
+				case 2:
+					Debug.Log ("Clicked Load Game");
+					break;
+				case 3:
+					Debug.Log ("Clicked Options");
+					break;
+				case 4:
+					Debug.Log ("Clicked Quit Game");
+					Application.Quit();
+					break;
+				}
+			}
+		}
+	}
+}
